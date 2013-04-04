12107cee91ee74abff4117e1f4255d68dad911e8
Brandon Buck <lordizuriel@gmail.com>
2013-04-04 02:54:07 -0500

    Add some basic account accessing
    Set up basic account accesses and checks and attempt to show the
    compose tweet view.

---
+++ BasicTwitter/BTBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTBaseViewController.h
+//  BasicTwitter
+//
+//  Created by Brandon Buck on 4/4/13.
+//  Copyright (c) 2013 Brandon Buck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTBaseViewController : UIViewController
+
+@end
---
+++ BasicTwitter/BTBaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  BTBaseViewController.m
+//  BasicTwitter
+//
+//  Created by Brandon Buck on 4/4/13.
+//  Copyright (c) 2013 Brandon Buck. All rights reserved.
+//
+
+#import "BTBaseViewController.h"
+
+@interface BTBaseViewController ()
+
+@end
+
+@implementation BTBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
